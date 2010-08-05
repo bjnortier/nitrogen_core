ec3ff839b418807fa7b10df79b4143e2efcf958b
Benjamin Nortier <bjnortier@gmail.com>
2010-08-05 16:45:49 +0100

    Initial commit

---
+++ src/actions/action_effect.erl
@@ -0,0 +1,17 @@
+% Nitrogen Web Framework for Erlang
+% Copyright (c) 2008-2010 Rusty Klophaus
+% See MIT-LICENSE for licensing information.
+
+-module (action_effect).
+-include_lib ("wf.hrl").
+-compile(export_all).
+
+render_action(Record) ->
+    #jquery_effect {
+        type=effect,
+        anchor = Record#effect.anchor,
+        target = Record#effect.target,
+        effect = Record#effect.effect,
+        options = Record#effect.options,
+        speed = Record#effect.speed
+    }.
---
+++ src/actions/action_remove_class.erl
@@ -0,0 +1,14 @@
+% Nitrogen Web Framework for Erlang
+% Copyright (c) 2008-2010 Rusty Klophaus
+% See MIT-LICENSE for licensing information.
+
+-module (action_remove_class).
+-include_lib ("wf.hrl").
+-compile(export_all).
+
+render_action(Record) ->
+    #jquery_effect {
+        type=remove_class,
+        class = Record#remove_class.class,
+        speed = Record#remove_class.speed
+    }.
